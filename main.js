bd003148dbada412b66eeb6093ff230af679c098
bonvi83 <bonvi83@gmail.com>
2024-02-29 18:05:09 +0100

    change struttura e add array

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      title: "ToDo List",
+      title: "Fotografica analogica",
+      todoList: [
+        `borsa attrezzatura`,
+        `controllo meccanico avanzamento rollino`,
+        `pulizia obiettivi`,
+        `rollini Kodak`,
+        `rollini Il Ford`,
+        `contenitori per sviluppo`,
+        `liquido sviluppo`,
+        `liquido STOP`,
+        `liquido fissativo`,
+        `contenitori pellicola`,
+      ],
     };
   },
 });
