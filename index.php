b3d90b43a6d2ed69aaac487edc1f291fd50e40c3
bonvi83 <bonvi83@gmail.com>
2024-02-29 15:33:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+require_once "./functions.php";
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- CDN Bootstrap -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+      crossorigin="anonymous"
+    />
+
+    <!-- CDN VueJS -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+
+    <title>ToDo List</title>
+  </head>
+
+  <body>
+    
+  </body>
+</html>
